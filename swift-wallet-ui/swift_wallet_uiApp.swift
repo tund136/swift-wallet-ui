e94c32626422d337e99e070fb7d0fe8222cfd9d8
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-08 20:56:50 +0700

    Set up the project

---
+++ swift-wallet-ui/swift_wallet_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_wallet_uiApp.swift
+//  swift-wallet-ui
+//
+//  Created by Danh Tu on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_wallet_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
